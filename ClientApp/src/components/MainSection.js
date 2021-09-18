ddd43964d2087606788d0555e8b739d939511f02
iamkeryn <kerynroos1501@gmail.com>
2021-09-18 17:55:58 +0200

    clear

---
+++ ClientApp/src/components/MainSection.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../App.css';
+import './MainSection.css';
+
+function MainSection() {
+  return (
+    <div className='main-container'>
+      //<img src='/images/home.png' />
+      <h1>XYZ Recruitment Company</h1>
+      <h2>Bridging the gap</h2>
+
+      <p>Welcome to XYZ! We specialise in bridging the gap between graduates and the workplace. </p>
+      <div className='main-btns'>
+        
+
+      </div>
+    </div>
+  );
+}
+
+export default MainSection;
